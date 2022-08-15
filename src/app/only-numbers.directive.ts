91ff0fd03fa52150631e030bc424a95511c398ea
mohammadghaliah <m.ghaliah98@gmail.com>
2022-08-15 23:53:43 +0300

    3 files modified, 1 added, 1 deleted

---
+++ src/app/only-numbers.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[AcceptOnlyNumbers]',
+})
+export class AcceptOnlyNumbers {
+  constructor(private el: ElementRef) {}
+
+  @Input() AcceptOnlyNumbers: boolean;
+
+  @HostListener('keypress', ['$event'])
+  onInput(event: any) {
+    debugger;
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(
+      event.which ? event.which : event.keyCode
+    );
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+      return false;
+    }
+    return true;
+  }
+
+  @HostListener('paste', ['$event'])
+  onPaste(event: ClipboardEvent) {
+    let dataToPaste = event.clipboardData.getData('text');
+    const regex = new RegExp(/^[\0-9]+$/);
+    if (regex.test(dataToPaste)) {
+      return dataToPaste;
+    } else {
+      event.preventDefault();
+    }
+  }
+}
